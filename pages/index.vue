802e5a917ff31c2c25536db61df41cc451a7d708
Yossi Abramov <josephabramov90@gmail.com>
2020-05-26 19:25:00 +0300

    main page title change

--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,18 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        SASSY-Boilerplate
-      </h1>
-      <h2 class="subtitle">
-        A Nuxt.js boilerpate with some modules I use frequently.
-      </h2>
+      <h1 class="title">SASSY-Boilerplate</h1>
+      <h2 class="subtitle">A Nuxt.js boilerpate.</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style>
@@ -44,8 +27,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
