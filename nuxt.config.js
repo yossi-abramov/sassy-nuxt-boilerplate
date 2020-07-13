dfa967d898a64a0f98bde6592d46fcbe3224a111
Yossi Abramov <josephabramov90@gmail.com>
2020-07-13 22:45:13 +0300

    Upgraded nuxt version to 2.13.3, added robots module and google tag manager module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,17 @@
 export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
   mode: "universal",
   /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "static",
+  /*
    ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
     title: process.env.npm_package_name || "",
@@ -11,71 +21,69 @@ export default {
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
-      }
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
   css: ["@assets/scss/app.scss"],
   /*
    ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
    */
   plugins: [],
   /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    "@nuxtjs/style-resources", // TODO: Remove when upgrading to nuxt 2.13+
-    "@nuxt/components" // Doc: https://github.com/nuxt/components
-  ],
+  buildModules: ["@nuxtjs/style-resources", "@nuxtjs/gtm"],
   /*
-   ** Do not import actual styles. Use this module only to import variables, mixins, functions (et cetera) as they won't exist in the actual build. Importing actual styles will include them in every component and will also make your build/HMR magnitudes slower. Do not do this!
-   Doc: https://github.com/nuxt-community/style-resources-module
-   */
+    ** Do not import actual styles. Use this module only to import variables, mixins, functions (et cetera) as they won't exist in the actual build. Importing actual styles will include them in every component and will also make your build/HMR magnitudes slower. Do not do this!
+    Doc: https://github.com/nuxt-community/style-resources-module
+  */
   styleResources: {
     // sass: [],
-    scss: ["@assets/scss/_variables.scss"]
+    scss: ["@assets/scss/_variables.scss"],
     // less: [],
     // stylus: []
   },
-  /**
-   * Allow auto import components for Nuxt.js 2.10+
+  /** Google Tag Manager Options
+   *  Doc: https://github.com/nuxt-community/gtm-module
    */
-  components: true,
+  gtm: {
+    id: "GTM-XXXXXXX",
+  },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/sitemap", "@nuxtjs/robots"],
   /** Sitemap options
    * Doc: https://www.npmjs.com/package/@nuxtjs/sitemap
    */
   sitemap: {
-    hostname: "https://mysite.com"
+    hostname: "https://mysite.com",
   },
-
-  /*
-   ** Nuxt.js modules
+  /** Robots options
+   * Doc: https://www.npmjs.com/package/@nuxtjs/robots
    */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios",
-    "@nuxtjs/sitemap"
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
+  robots: {
+    /* module options */
+  },
   /*
    ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {},
+  },
 };
